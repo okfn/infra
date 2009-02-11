37ccb625a628d7191ff96b2eaa3403cb95cd3bb2
Rufus Pollock <rufus.pollock@okfn.org>
2009-02-11 17:46:40 +0000

    [okfn.org/geo][m]: (old: 3y) add core geo stuff (html + basic php).

---
+++ var/www/okfn.org/geo/manifesto.it.php
@@ -0,0 +1,194 @@
+<html><head>
+<link rel="stylesheet" href="opengeo.css">
+<title>Accesso libero ai dati cartografici raccolti dallo Stato</title></head>
+<body>
+<div id="content">
+<p class="notes"><a href="manifesto.php">[ English ]</a> |
+Italiano | <a href="manifiesto.es.php">[ Espa&ntilde;ol ]</a></p>
+
+
+<h1>Accesso libero ai dati cartografici raccolti dallo Stato</h1>
+
+<p>
+	Crediamo che i dati cartografici raccolti dallo stato debbano
+	essere liberamente disponibili per i cittadini.
+	<a href="#signup">Firma</a> in fondo
+	alla pagina per supportare questo manifesto.
+</p>
+
+<h2>Come Funzionerebbe</h2>
+
+<h3>Tutti i dati geografici collezionati dai governi dovrebbero essere
+aperti, cioe' liberamente distribuibili e riutilizzabili con una
+licenza ShareAlike.</h3>
+
+<p>
+	I dati geografici sono un bene pubblico. Un accesso libero
+	ad essi, sotto una licenza "Commons"
+	(condividi allo stesso modo), e' il modo
+	migliore per consentire ad industrie e cittadini di sfruttarne
+	a pieno le potenzialita'. Allo stesso tempo una tale disposizione,
+	richiedendo agli utilizzatori di ridistribuire aggiornamenti e
+	migliorie, porta ad una maggiore disponibilita' e qualita' dei
+	dati stessi, a costi minori.
+</p>
+
+<p>
+	Iniziative relative includono il modello di <a href="http://www.okfn.org/geo/geodata_cc_license_draft.rtf">non-profit
+	open geodata license</a> realizzato da <a href="http://urbantapestries.net/">Urban Tapestries</a> (en) in
+	collaborazione con <a href="http://creativecommons.org.uk/">Creative Commons UK</a>.  La licenza e' stata
+	sottoposta all' Agenzia Nazionale Inglese, la 
+	<a href="http://www.ordnancesurvey.gov.uk/">Ordnance Survey</a> (en),
+	come base per una collaborazione aperta.
+</p>
+
+<h3>I progetti di mappatura online che creano dati geografici liberamente
+riutilizzabili dovrebbero offrire una licenza aperta compatibile.</h3>
+
+<p>
+	Il progetto OpenStreetmap offre la sua crescente banca dati 
+	sotto una licenza "Attribution-Sharealike", che consente ogni
+	tipo di riuso dei dati fintanto i lavori derivati rimangano a
+	loro volta di pubblico dominio.
+</p>
+
+<h3>Dovrebbero essere adottati formati standard per lo scambio e la
+descrizione di dati geografici.</h3>
+
+<p>
+	L' <a href="http://opengis.org/">Open Geospatial Consortium</a> (en)
+	ha creato una famiglia di standard per lo scambio aperto di dati
+	geografici su Internet, e i metadati standard del
+	<a href="http://www.w3.org/RDF/">World Wide Web Consortium</a> (en)
+	sono generalmente utilizzati.
+</p>
+
+<h3>In ultima analisi, tutte le informazioni raccolte dallo stato dovrebbero
+essere disponibili in maniera aperta, in un formato strutturato e
+leggibile dalle macchine.</h3>
+
+<h2>In Che Modo Questo Aiuterebbe</h2>
+
+<h3>Dati aperti vuol dire dati migliori e piu' economici.</h3>
+
+<p>
+	Quando piu' produttori di informazione hanno l'opportunita'
+	di contribuire all'accuratezza e all'aggiornamento dei dati,
+	la qualita' aumenta. Quando piu' organizzazioni hanno la
+	possibilita'di offrire servizi di informazione spaziale
+	competitivi, i prezzi scendono.
+</p>
+
+<p>
+	Lo Stato Italiano paga grosse societa' di proprieta' dello Stato
+	stesso o a partecipazione statale per produrre dati cartografici
+	che vengono poi venduti nuovamente allo Stato con licenze e
+	sublicenze; una falsa economia che esclude i cittadini ordinari e
+	le organizzazioni no-profit impossibilitati a sostenere i prezzi
+	monopolistici delle licenze.
+</p>
+
+<h3>I dati geografici aperti aiutano l'economia.</h3>
+
+<p></p>
+	Negli Stati Uniti, la politica nazionale mette tutte le
+	informazioni geospaziali raccolte dal governo nel pubblico
+	dominio, senza costo e senza restrizioni d'uso.  Questo abbatte
+	i costi della ricerca e dello sviluppo, e l'innovazione da parte
+	dell'industria e degli individui crea un'attivita' economica.
+<p></p>
+
+<h3>L'accesso libero ai dati geografici aiuta la democrazia e la
+partecipazione dei cittadini.</h3>
+
+<p>
+	Il 75-80% delle informazioni generate dal governo ha una
+	componente spaziale.  Perche' l'informazione del settore pubblico
+	sia efficace, essa deve avere la piu' ampia disponibilita'
+	possibile. Le leggi europee sulla liberta' di informazione
+	enfatizzano fortemente la disponibilita' di dati geografici
+	<a href="http://europa.eu.int/celex/cgi/sga_rqst?SESS=14772%21CTXT=8%21UNIQ=9%21APPLIC=celexext%21FILE=VISU_vitxt_8__1%21DGP=0%21VI_all0#texte">[1]</a>.
+</p>
+
+<p>
+	I dati geografici sono alla base di servizi sviluppati da
+	gruppi di informazione civica no-profit, come 
+	<a href="http://writetothem.com/">Write To Them</a> (en)
+	e <a href="http://theyworkforyou.com/">They Work For You</a> (en).
+	Sono indispensabili per i progetti di
+	analisi delle informazioni pubbliche e per renderle maggiormente
+	disponibili al pubblico interessato.
+</p>
+
+<a name="signup">
+<hr>
+</a>
+
+<p>
+	Se condividi queste dichiarazioni, per favore aggiungi il
+	tuo nome.  Stiamo raccogliendo firme di utilizzatori di dati
+	spaziali e sviluppatori di software GIS da tutto il mondo per
+	aiutare a dimostrare un'ampio supporto per le politiche di
+	accesso libero ai dati geografici.
+</p>
+
+<h3>
+ persone hanno firmato il manifesto</h3>
+
+<form method="post" action="http://okfn.org/geo/signup.php">
+<input name="lang" value="it" type="hidden">
+<table>
+<tbody><tr><td>Nome:
+</td><td>
+<input name="name" type="text">
+</td></tr>
+
+<tr><td>Email:
+<br><span class="notes">non la pubblicheremo, ma potremmo voler restare in contatto</span>
+</td><td>
+<input name="mbox" type="text">
+</td></tr>
+
+<tr><td>
+Organizzazione:
+</td><td>
+<input name="org" type="text">
+</td></tr>
+
+<tr><td>
+Sito Web:
+</td><td>
+<input name="url" type="text">
+</td></tr>
+
+<tr><td>
+Nazione:
+
+</td>
+<td>
+<select name="country"><option value="Italy">Italy</option><option value="United Kingdom">United Kingdom</option><option value="Afghanistan">Afghanistan</option><option value="Albania">Albania</option><option value="Algeria">Algeria</option><option value="American Samoa">American Samoa</option><option value="Andorra">Andorra</option><option value="Angola">Angola</option><option value="Anguilla">Anguilla</option><option value="Antarctica">Antarctica</option><option value="Antigua, Barbuda">Antigua, Barbuda</option><option value="Argentina">Argentina</option><option value="Armenia">Armenia</option><option value="Australia">Australia</option><option value="Austria">Austria</option><option value="Azerbaijan">Azerbaijan</option><option value="Bahamas">Bahamas</option><option value="Bahrain">Bahrain</option><option value="Bangladesh">Bangladesh</option><option value="Barbados">Barbados</option><option value="Belarus">Belarus</option><option value="Belgium">Belgium</option><option value="Belize">Belize</option><option value="Benin">Benin</option><option value="Bermuda">Bermuda</option><option value="Bhutan">Bhutan</option><option value="Bolivia">Bolivia</option><option value="Bosnia-Herzegovina">Bosnia-Herzegovina</option><option value="Botswana">Botswana</option><option value="Brazil">Brazil</option><option value="British Indian Ocean Territory">British Indian Ocean Territory</option><option value="British Virgin Islands">British Virgin Islands</option><option value="Brunei">Brunei</option><option value="Bulgaria">Bulgaria</option><option value="Burkina Faso">Burkina Faso</option><option value="Burma">Burma</option><option value="Burundi">Burundi</option><option value="Cambodia">Cambodia</option><option value="Cameroon">Cameroon</option><option value="Canada">Canada</option><option value="Canary Islands">Canary Islands</option><option value="Cape Verde">Cape Verde</option><option value="Cayman Islands">Cayman Islands</option><option value="Central Africa">Central Africa</option><option value="Chad">Chad</option><option value="Chile">Chile</option><option value="China">China</option><option value="Christmas Island">Christmas Island</option><option value="Cocos Island">Cocos Island</option><option value="Colombia">Colombia</option><option value="Comoros">Comoros</option><option value="Congo">Congo</option><option value="Cook Islands">Cook Islands</option><option value="Costa Rica">Costa Rica</option><option value="Croatia">Croatia</option><option value="Cuba">Cuba</option><option value="Cyprus">Cyprus</option><option value="Czech Republic">Czech Republic</option><option value="Democratic Republic of Congo">Democratic Republic of Congo</option><option value="Denmark">Denmark</option><option value="Djibouti">Djibouti</option><option value="Dominica">Dominica</option><option value="Dominican Republic">Dominican Republic</option><option value="East Timor">East Timor</option><option value="Ecuador">Ecuador</option><option value="Egypt">Egypt</option><option value="El Salvador">El Salvador</option><option value="Equatorial Guinea">Equatorial Guinea</option><option value="Eritrea">Eritrea</option><option value="Estonia">Estonia</option><option value="Ethiopia">Ethiopia</option><option value="FYRO Macedonia">FYRO Macedonia</option><option value="Falkland Islands">Falkland Islands</option><option value="Faroe Islands">Faroe Islands</option><option value="Fiji">Fiji</option><option value="Finland">Finland</option><option value="France">France</option><option value="Gabon">Gabon</option><option value="Gambia">Gambia</option><option value="Georgia">Georgia</option><option value="Germany">Germany</option><option value="Ghana">Ghana</option><option value="Gibraltar">Gibraltar</option><option value="Greece">Greece</option><option value="Greenland">Greenland</option><option value="Grenada">Grenada</option><option value="Guadeloupe">Guadeloupe</option><option value="Guam">Guam</option><option value="Guatemala">Guatemala</option><option value="Guinea">Guinea</option><option value="Guinea-Bissau">Guinea-Bissau</option><option value="Guyana">Guyana</option><option value="Haiti">Haiti</option><option value="Honduras">Honduras</option><option value="Hong Kong">Hong Kong</option><option value="Hungary">Hungary</option><option value="Iceland">Iceland</option><option value="India">India</option><option value="Indonesia">Indonesia</option><option value="Iran">Iran</option><option value="Iraq">Iraq</option><option value="Ireland">Ireland</option><option value="Israel">Israel</option><option value="Ivory Coast">Ivory Coast</option><option value="Jamaica">Jamaica</option><option value="Japan">Japan</option><option value="Jordan">Jordan</option><option value="Kazachstan">Kazachstan</option><option value="Kenya">Kenya</option><option value="Kiribati">Kiribati</option><option value="Kuwait">Kuwait</option><option value="Kyrgyzstan">Kyrgyzstan</option><option value="Laos">Laos</option><option value="Latvia">Latvia</option><option value="Lebanon">Lebanon</option><option value="Lesotho">Lesotho</option><option value="Liberia">Liberia</option><option value="Libya">Libya</option><option value="Liechtenstein">Liechtenstein</option><option value="Lithuania">Lithuania</option><option value="Luxembourg">Luxembourg</option><option value="Madagascar">Madagascar</option><option value="Malawi">Malawi</option><option value="Malaysia">Malaysia</option><option value="Maldives">Maldives</option><option value="Mali">Mali</option><option value="Malta">Malta</option><option value="Marshall Islands">Marshall Islands</option><option value="Martinique">Martinique</option><option value="Mauritania">Mauritania</option><option value="Mauritius">Mauritius</option><option value="Mexico">Mexico</option><option value="Micronesia">Micronesia</option><option value="Moldova">Moldova</option><option value="Monaco">Monaco</option><option value="Mongolia">Mongolia</option><option value="Montserrat">Montserrat</option><option value="Morocco">Morocco</option><option value="Mozambique">Mozambique</option><option value="Namibia">Namibia</option><option value="Nauru">Nauru</option><option value="Nepal">Nepal</option><option value="Netherlands">Netherlands</option><option value="New Caledonia">New Caledonia</option><option value="New Zealand">New Zealand</option><option value="Nicaragua">Nicaragua</option><option value="Niger">Niger</option><option value="Nigeria">Nigeria</option><option value="Niue">Niue</option><option value="Norfolk Island">Norfolk Island</option><option value="North Korea">North Korea</option><option value="Norway">Norway</option><option value="Oman">Oman</option><option value="Pakistan">Pakistan</option><option value="Palau">Palau</option><option value="Palestine National Authority">Palestine National Authority</option><option value="Panama">Panama</option><option value="Papua New Guinea">Papua New Guinea</option><option value="Paraguay">Paraguay</option><option value="Peru">Peru</option><option value="Philippines">Philippines</option><option value="Pitcairn Islands">Pitcairn Islands</option><option value="Poland">Poland</option><option value="Polynesia">Polynesia</option><option value="Portugal">Portugal</option><option value="Puerto Rico">Puerto Rico</option><option value="Qatar">Qatar</option><option value="Reunion">Reunion</option><option value="Romania">Romania</option><option value="Russia">Russia</option><option value="Rwanda">Rwanda</option><option value="Saint Kitts and Nevis">Saint Kitts and Nevis</option><option value="Saint Lucia">Saint Lucia</option><option value="Saint Vincent and the Grenadines">Saint Vincent and the Grenadines</option><option value="Samoa">Samoa</option><option value="San Marino">San Marino</option><option value="Sao Tome">Sao Tome</option><option value="Saudi Arabia">Saudi Arabia</option><option value="Senegal">Senegal</option><option value="Serbia and Montenegro">Serbia and Montenegro</option><option value="Seychelles">Seychelles</option><option value="Sierra Leone">Sierra Leone</option><option value="Singapore">Singapore</option><option value="Slovakia">Slovakia</option><option value="Slovenia">Slovenia</option><option value="Solomon Islands">Solomon Islands</option><option value="Somalia">Somalia</option><option value="South Africa">South Africa</option><option value="South Korea">South Korea</option><option value="Spain">Spain</option><option value="Sri Lanka">Sri Lanka</option><option value="St. Helena">St. Helena</option><option value="Sudan">Sudan</option><option value="Suriname">Suriname</option><option value="Swaziland">Swaziland</option><option value="Sweden">Sweden</option><option value="Switzerland">Switzerland</option><option value="Syria">Syria</option><option value="Tadjikistan">Tadjikistan</option><option value="Taiwan">Taiwan</option><option value="Tanzania">Tanzania</option><option value="Thailand">Thailand</option><option value="Togo">Togo</option><option value="Tokelau">Tokelau</option><option value="Tonga">Tonga</option><option value="Trinidad and Tobago">Trinidad and Tobago</option><option value="Tunisia">Tunisia</option><option value="Turkey">Turkey</option><option value="Turkmenistan">Turkmenistan</option><option value="Tuvalu">Tuvalu</option><option value="Uganda">Uganda</option><option value="Ukraine">Ukraine</option><option value="United Arab Emirates">United Arab Emirates</option><option value="United States">United States</option><option value="Uruguay">Uruguay</option><option value="Uzbekistan">Uzbekistan</option><option value="Vanuatu">Vanuatu</option><option value="Vatican City State">Vatican City State</option><option value="Venezuela">Venezuela</option><option value="Vietnam">Vietnam</option><option value="Virgin Islands, British">Virgin Islands, British</option><option value="Virgin Islands, US">Virgin Islands, US</option><option value="Western Sahara">Western Sahara</option><option value="Yemen">Yemen</option><option value="Zambia">Zambia</option><option value="Zimbabwe">Zimbabwe</option></select>
+</td>
+</tr>
+<tr><td colspan="2">
+<input value="sottoscrivi" type="submit">
+</td></tr>
+</tbody></table>
+</form>
+
+<p>
+<a href="http://okfn.org/geo/signup.php">Visita la lista dei firmatari</a>
+(en)
+</p>
+
+<p>
+Per favore, aggiungi commenti e link nella <a href="http://okfn.org/wiki/OpenGeodataManifesto">pagina wiki</a> (en) sul Manifesto
+Per I Geodati Aperti.
+</p>
+
+<p>
+Per informazioni di base e link, visita le pagine relative ai <a href="http://okfn.org/wiki/OpenGeoData">Geodati Aperti</a> (en) sul
+wiki della <a href="http://okfn.org/">Open Knowledge Foundation</a> (en).
+</p>
+</div>
+
+</body></html>
