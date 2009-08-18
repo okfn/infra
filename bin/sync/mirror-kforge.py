abc3246cdfde8791a14350b710035ae439df0ccc
Rufus Pollock <rufus.pollock@okfn.org>
2009-08-18 18:55:37 +0000

    [bin/sync][xs]: (old: ?) minor improvements to mirror-kforge.py script.

--- bin/sync/mirror-kforge.py
+++ bin/sync/mirror-kforge.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import os
-excludes_file = '/home/okfn/bin/mirror/mirror-kforge-files.txt' 
+here = os.path.dirname(__file__)
+excludes_file = os.path.join(here, 'mirror-kforge-files.txt')
 src_host = 'us0.okfn.org'
 
-cmd1 = 'rsync -arvz --bwlimit=10 --delete -e "ssh -i /home/okfn/.ssh/okfn-rsync-key" okfn@%s:/home/kforge/ /home/kforge --include-from=%s' % (src_host, excludes_file)
+cmd1 = 'rsync -arvz --bwlimit=200 --delete -e "ssh -i /home/okfn/.ssh/okfn-rsync-key" okfn@%s:/home/kforge/ /home/kforge --include-from=%s' % (src_host, excludes_file)
 
 os.system(cmd1)
