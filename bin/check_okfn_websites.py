b8162be6728a8f2fc710ab6efbb5cc93c81d513e
Rufus Pollock <rufus.pollock@okfn.org>
2010-01-08 11:16:01 +0000

    [bin][s]: overhaul check_okfn_websites.py to use only python stdlib (no twill) and have up to date list of sites.

--- bin/check_okfn_websites.py
+++ bin/check_okfn_websites.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-import twill.commands as web
-
 import unittest
+import urllib2
 
 class TestSites(unittest.TestCase):
+    VERBOSE = True
 
     core_sites = [
             'http://www.okfn.org/',
@@ -23,22 +23,40 @@ class TestSites(unittest.TestCase):
             'http://www.knowledgeforge.net/',
             'http://www.kforgeproject.com/',
             'http://www.openshakespeare.org/',
-            'http://demo.openshakespeare.org/',
+            'http://blog.openshakespeare.org/',
             'http://www.ckan.net',
-            'http://db.publicdomainworks.net/',
+            'http://www.publicdomainworks.net/',
             'http://www.openeconomics.net/',
-            'http://isitopen.ckan.net/',
+            'http://www.isitopendata.org/',
+            'http://www.wheredoesmymoneygo.org/',
+            'http://www.openmilton.org/',
             ]
 
+    def _print(self, msg):
+        if self.VERBOSE:
+            print(msg)
+
+    def _check_site(self, url):
+        self._print('Visiting: %s' % url)
+        try:
+            res = urllib2.urlopen(url)
+            code = res.code
+        except urllib2.HTTPError as e:
+            code = e.code
+            reason = e.read()
+        except urllib2.URLError, e:
+            code,info = e.reason
+        assert code in [200,302], '\tFAILED (%s): %s' % (code,url)
+        # o/w ok
+        self._print('\tOK: %s' % url)
+
     def test_okfn(self):
-        for site in self.core_sites:
-            web.go(site)
-            web.code(200)
+        for url in self.core_sites:
+            self._check_site(url)
 
     def test_others(self):
-        for site in self.other_sites:
-            web.go(site)
-            web.code(200)
+        for url in self.other_sites:
+            self._check_site(url)
 
 if __name__ == '__main__':
     unittest.main()
