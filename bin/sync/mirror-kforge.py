f5d7f1853d30ca3237614444f3b6012d47b267e8
Rufus Pollock <rufus.pollock@okfn.org>
2010-01-08 12:13:42 +0100

    [misc][s]: minor changes to bin/sync/mirror-kforge* (change to knowledgeforge.net and change excludes) and minor addition to PRODUCTION_HOWTO.txt.

--- bin/sync/mirror-kforge.py
+++ bin/sync/mirror-kforge.py
@@ -3,8 +3,8 @@
 import os
 here = os.path.dirname(__file__)
 excludes_file = os.path.join(here, 'mirror-kforge-files.txt')
-src_host = 'us0.okfn.org'
+src_host = 'knowledgeforge.net'
 
-cmd1 = 'rsync -arvz --bwlimit=200 --delete -e "ssh -i /home/okfn/.ssh/okfn-rsync-key" okfn@%s:/home/kforge/ /home/kforge --include-from=%s' % (src_host, excludes_file)
+cmd1 = 'rsync -arvz --delete -e "ssh -i mirror_key" root@%s:/home/kforge/ /home/kforge --include-from=%s' % (src_host, excludes_file)
 
 os.system(cmd1)
