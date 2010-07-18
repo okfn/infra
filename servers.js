f0d4447bf6dda35ca5a3cfc0a397f16ea8746dd1
James Casbon <james.casbon@popgentech.com>
2010-07-18 18:10:32 +0100

    Add servers.js bases on google spreadsheet

---
+++ servers.js
@@ -0,0 +1,74 @@
+{
+    "us3": {
+        "created": "2010-02", 
+        "notes": null, 
+        "location": "us-east-1d", 
+        "provider": "aws", 
+        "cost per month": null, 
+        "type": "ec2", 
+        "spec": "small"
+    }, 
+    "us2": {
+        "created": "2009-10", 
+        "notes": null, 
+        "location": "us-west", 
+        "provider": "dreamhost", 
+        "cost per month": null, 
+        "type": "shared", 
+        "spec": "small"
+    }, 
+    "us1": {
+        "created": "2009-10", 
+        "notes": null, 
+        "location": "us-east-1d", 
+        "provider": "aws", 
+        "cost per month": null, 
+        "type": "ec2", 
+        "spec": "small"
+    }, 
+    "eu4": {
+        "created": "2010-04", 
+        "notes": "i-38f1414f", 
+        "location": "us-west-1b", 
+        "provider": "aws", 
+        "cost per month": "100", 
+        "type": "ec2", 
+        "spec": "large"
+    }, 
+    "eu1": {
+        "created": "2008", 
+        "notes": null, 
+        "location": "uk", 
+        "provider": "bytemark", 
+        "cost per month": null, 
+        "type": "dedicated-virtual", 
+        "spec": null
+    }, 
+    "eu0": {
+        "created": "2007", 
+        "notes": null, 
+        "location": "de", 
+        "provider": "hentzner", 
+        "cost per month": "49", 
+        "type": "dedicated", 
+        "spec": ""
+    }, 
+    "eu3": {
+        "created": "3/15/2010", 
+        "notes": null, 
+        "location": "eu-west-1b", 
+        "provider": "aws", 
+        "cost per month": null, 
+        "type": "ec2", 
+        "spec": "small"
+    }, 
+    "eu2": {
+        "created": "2010", 
+        "notes": null, 
+        "location": "eu-west-1b", 
+        "provider": "aws", 
+        "cost per month": null, 
+        "type": "ec2", 
+        "spec": "small"
+    }
+}
