934535ded31862a3e0bc2f7cd74e3f211b332d64
Rufus Pollock <rufus.pollock@okfn.org>
2010-07-18 18:10:40 +0100

    [ssh_keys.js][s+]: json file of public ssh keys of main dev/sysadmin people (for use with fabric script etc).

---
+++ ssh_keys.js
@@ -0,0 +1,56 @@
+{
+    "groups":
+    {
+        "okfn-sysadmin": ["rgrp", "pudo", "ww", "mk270", "casbon", "benosteen" ]
+    },
+    "users":
+    {
+        "mk270":
+        {
+            "name": "Martin Keegan",
+            "key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEA0QvlAkGAQvqf2SM1a6qbPdzare4sTiN8DiKgog2xy87VeJxoDW6GUxy5J/794vDyJOW7CbewSC7IiFTBLSolOQRh1AJs7kbLfFfOwE/LSwpCP396jRy0p+wDUrGi2nEiFccynY8nWblsJk1bS59L1fjfxMln6WvakiCU6JbVUvk= mkeegan@goatnut"
+        },
+        "johnbywater":
+        {
+            "name": "John Bywater",
+            "key": "ssh-dss AAAAB3NzaC1kc3MAAACBAIJ+izF4nurowTHcnPiS4YqmQ4VQhXgk5x5OE2MMTnvxynwTm7CSH3Nm6kN3QJbQsGm4duqZEm91qzTTF+mipsdUH3/QEhSU6VHMRVPSlqRbaPiHciFhoHGpWBOTSM0G45JTFZOY8Zk5SKD9/IlRQ9yqyXTc6IkqLA2b3ICd85ppAAAAFQDbBpitIk/jDsuVm9O+1j3Wc4pBMQAAAIAM0UNPsYw89tcgCgpuhpp6W4/lhaO/Gyr7E2PkJ/1vs/REH3gnVmO28LaNa6x3ot6z/nmu3XgyPOeiLxLWS02qoSlM2Ak77oWzJnG2AxLaKU69/0bvrWUQkoATYAO5uF9T/Q5kOn9QcLRpDx6QdY1Mme037cnum9LnPcp4sDvgmQAAAIA4Rlqe6bINx+D536XOcCKRosg6I7D11p2cgqQoh7+W4ztINnr6L8edRBWQ/DYiBqEAIhIXcbKZKOjDieHFvad/F5JJ2G9P1Gn4ITMgkMFmg00671WHunpPX15cywcxmX7a5XDMMc8nx1r8Ku2ueBuAp9ni4N/jqkbAmNANMnbAyA== john@hawthorn"
+        },
+        "casbon" :
+        {
+            "name": "James Casbon",
+            "key": "ssh-dss AAAAB3NzaC1kc3MAAACBAP4V5RFd6c4f2TXPl2rVuKtrSm8F7XdIfyyQ/+NIHY0c2x2jEe5C737rEUTFPF0MKxo55EOU8XGK6UOGitOB2jjt4/8iMj5HCVjqU357y0gVhH33IGCd1Euk5A7Tb5viR5rpEmSdz6ExHPFTdnGf2/MpSBhY6yxe3P+Ok2NZ/IXHAAAAFQCVmPCekKiU0XpSrCKIXGepp9VdKwAAAIB6V9jW5Y+P3zMtC4nUcTmx/8/220Jk4JamntAswbINJeyCg4+0c62OkGbrDmYMGj/isPoSvDs+RkjisRJaohP5r3FuqpdmByLvu4Bt+On/6cnAiTxuHOpPb/mvYzTYbxrhI1Ox1sKDR6uIA5Dx+jeFiIeHf64DFhVUCUfyJTIjLgAAAIEA04EIYZjtF3yv2guM8KNVVbc0vz/LPu6pqNj0nN2qV53DGrYQswj7uk2yLh2BqUMyisB5BWGNRa93iuc6LHihUYmdrZu1gZFxb7mfOAsq2LmRmZgMWFmrjymsbYTPeOQKrsfRLyH92Bz2bn+GD34Izh/lTPi/eFsRhQ+TrByBP30= james@Flapjack.local"
+        },
+        "ww":
+        {
+            "name": "William Waites",
+            "key": "ssh-dss AAAAB3NzaC1kc3MAAACBAP9tJ0mhWoq2qnROdg2EfVpsXw4diyXi+DTq4TXmjpjWgsebviEA8zJApGsLEViEQuNEtrSZBqncgTVdlDcCmJckFKEyPAscujK8F1a3FyIbjMRF4Ewgj94PeINZGuU35iqyKSTPn+uLtLI19wJyeQ11JLFbSPYo9gjdP+gsgcHJAAAAFQCC6KJn5Sq0U9Kk8N5Xm0bkz+C9nwAAAIEAzSwNYM1KN9UQTievH7r7Cjzof2WLyqVwZ+eCOGRPIKmd0GyBpkJ+5SW43EXDzeWHqTucsm3TWiT+di7Oyh95gk58tXjhmer7pnAP9kvlmbz8L2JOYzf1fdTXodobvjk+ZvGeqr57SBaoFJi3NMlgXyuBSBZMkZ1VyEE1jlF7y+8AAACBAIieUK4Po0HpcnqO/LskAHwkK283NUW/CwVngZyXwbFXQgB0+Um8pynhXnmDtoekVV+je+W6EyEHrNqBMDO0zf99l/6PtczpL1Lz3qodQOVEpcQtK2iAL3wgFWjpgSAizvsEKZKOm5+DfJ1hGfxea5P/7D4vIDiL4aKio48ysUXr ww@irl.styx.org"
+        },
+        "rgrp":
+        {
+            "name": "Rufus Pollock",
+            "key": "ssh-dss AAAAB3NzaC1kc3MAAACBAN3MM1dL+9IqSU7fP14IxvZI2bS0whPz6LOTBEgLIW+KqtS8koIC3BU1cymo2nF1mbMM1d7xSh09RbHR8feT9W85pn4slMjUC8yMoeaODku038mMSqK2j6vhFm/DLUEqNIYpn34ZdykvO8hyVmoy7lJqcAjiGbP7WfQjwMYFHOZpAAAAFQCwcdhE6xj3p+W5P1l5ZrmPFq0LKwAAAIEA0khO9JR4QHchzHDUTueyoWXEfH14iHU9nHCRoJZViV1I6mTMSgPqAWx+RYIWN/peuyWxTvgJTRl6B+NetMi35nkY+uVNe4PfFVsGqWzXRySsQUKg2OpWc465nHlJTB7lz6GQoo5zBKP55gki8PJpbjLL4LhnmTCirl7BTTfCtfYAAACAWXZOAqKhudB7TcZQU9AJK3zVtjYtCm/eTVrXQwvXMtnU3SP8LioJtXKDpw9xRhcSekvvA6o03HJ2KnNf4robXbb8eb++6yuXH3FcSUlK5lZeDtFVrbN3nbppcZzWRSNGwavPsTMfQDANcbuTmEAsb+2wwHmuqyITpwciG3iIyCs= rgrp@gimmel"
+        },
+        "pudo":
+        {
+            "name": "Friedrich Lindenberg",
+            "who": "OKF Germany Tech Lead. Manager of de.ckan.net",
+            "key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAs7bj/WcbkcMNpdYbgrCIlTo5EXRkj+ghKBKgqHAQDX0+eHf+wwvxB8FgIXhcWmR0rsAbSR5thQ4Iv4io0NqkoIAko5GivCDdWHuKVQ7e3+nTsuiQUucfPL92jC0GVd17zJ06W4Kf+g0f5fIrdzaavNIvCdB/Ngmz8BrhfYJAsyJHXaUI7endQxag+aKYIOumi7lsVEBa8ggJPiDtzF2hJSNMSQtuO0/rxQu1ItKrxDruZYIwKjDLBY2MxFnOyEC5TsHAUJd+bAV1gy+BfdJg72MifTuM8lWlseEEmO/WmH94dKH0PeHfc4VP9MjCF9ykZqo5FOkhDJP3Quv9/n4Lcw== fl@dfn105.rz.tu-ilmenau.de"
+        },
+        "benosteen":
+        {
+            "name": "Ben O'Steen",
+            "key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwoofIGaz7mqzZT+lkfD076iN21odpswp7wVFDYkSjce97p/+Raj3Ns62P+6+/DezP4Sjp3Fyi+AC6KM/jSzoyPcgnRauhvYrH7kOMyWKarJhIZFA9lyQ0OOt+yeZdZfxsCYALC674TQ6chzDaAZ/SORe84biUzrkbxb3kFie1XMNyIbFsl91BRZ6h4C5aov5Eqh2kB8vI1rI/iqk7AtH7H1/oBpmMOOiq4alW0/YR8AMumqULQabhOo4kjRxa96BpNnzzZRllapoZARKVwtdk4kFFd6rS45hMbU0l//EkW/OTF0+t/uRDxeyVflkb6YS72JesKMEfFiz+cWc0SNj/Q== bosteen@gmail.com"
+        },
+        "dread":
+        {
+            "name": "David Read",
+			"who": "CKAN dev",
+            "key": "ssh-dss AAAAB3NzaC1kc3MAAACBAOWojLyr0xJTKd/WWIGCP2Lkjkdt8nRUQXBVEBzeT8OJRXEF9grmqscdzHLedJoR/PyZNuLRT+1aD/fN7dF3nhdHYw/kseSFI1ZGss2btfxIEFGaWTmblu5yHOd+j8ws67aSQcixpyjOEwym07/l29n0Z2gqtiS01n0mj4R4VIBZAAAAFQCEcAW+SdTml9z97xtwOHd2pMiP8wAAAIEAzkoKP91JIYiwnCTRzO2M+9IT6FBvhUqoaZqTs6aeTJWvzFBoPPlt9T63V5Guke0DZcTW53QT7FE+NhoEt+FFAul09FhbjEhXVKbBYLMmx5/sQ54heRJEJqHYqEullJlG1mubxx29x/baiILLPp0JZf9eSpOjyVQf97YEKHmOphwAAACALTlUKABwoZbFEUNedLxib8F4NA+M2g6ynDADpJP3l2E9xqoBEZqeP2LoMci5c1ebTDeY9I7P6AQSYkoi9x/crcvb8aaTSUjCkEwli+QDNSDQo8b+x6HIvO+AvO0l5rR12QNiLckEJ7lP0+BLDidDdtqs1Izy8qEWU1KJi0AGxjY= dread@dread-laptop"
+        },
+		"gjhiggins":
+		{
+			"name": "Graham Higgins",
+			"key": "ssh-dss AAAAB3NzaC1kc3MAAACBAIa9cT2n/CjA91FstDZbWNtu2Zd+WyIO1PbUa8djtituMvOfcweqIU5YhO9CK6qmFbzF9PvAAsQ3pDI920rcEdHsdCszyy49Pu5OoYUHo0BbBpx41Kl7doXZ81gGWOx3C86klsdUCnqbdRM0yj1UHQ9ET9vS6evxJmrmGgiSRP8tAAAAFQCqRjqSZ74UTw53Wd3FRpAW4bXTVwAAAIAwJxzme1JZ5rmJLZAr3k/LyMK8eOpyMR9HtYj7a+HD7U7ZXUyz+K11u1iZkQTDD+IvwTzzWluxbNwUebv+KaZ+0GG2y4AL9Z9sq/HYfxF9j1LC7otC4+CnfSo4CQmF8fvKqjdJQ+D6luOyuLM5kYc/6fwkuS7TLS2A0N6Zny0jTAAAAIBpUFST8FSVCzoAXD+CC4WL+N/pr0LGa1R3wu5EO5gprX8dRUjBaG2UD9gVmpTcyaWJX63YMg0cTWj5Ipb5EiGUydoaLSjS5lz7Y+CcjGRhWD/ZHSB+OMUC6gSrVXLlJPtELOmMUy/tF5tRzl71jIWdX/oiFMWp8MCBp+d029ezVQ=="
+		}
+    }
+}
