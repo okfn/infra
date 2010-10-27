fbe9daabc014479280c6e771f11e29c4749e7754
Rufus Pollock <rufus.pollock@okfn.org>
2010-10-27 15:45:49 +0100

    [bin/server_roles.py][s]: refactor and improve (role names).

--- bin/server_roles.py
+++ bin/server_roles.py
@@ -7,28 +7,19 @@ except:
 
 SERVERS_JS = os.path.join(os.path.dirname(__file__), '..', 'servers.js')
 
-PROPERTY_BLACKLIST = [
-    'created',
-    'cost per month',
-]
-
 def get_roles():
     """ parse server.js and return fabric role definitions 
     """
     servers = collections.defaultdict(list)    
     for hostname, props in json.load(file(SERVERS_JS)).items():
-        
         # fab requires a FQDN 
         hostname += '.okfn.org'
         
         servers['all'].append(hostname)
 
         for name, value in props.items():
-            if value and name not in PROPERTY_BLACKLIST: 
-                servers[value].append(hostname)
-            
-        
-        # servers[props.get('provider')].append(hostname)
+            if name in ['location', 'owner', 'provider']:
+                servers['%s::%s' % (name,value)].append(hostname)
     return dict(servers)
         
 if __name__ == '__main__':
