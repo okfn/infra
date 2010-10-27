c6c2e476bdeb530c099cb5563f1bdc55852965d4
Rufus Pollock <rufus.pollock@okfn.org>
2010-10-27 15:58:04 +0100

    [aws][m]: add snapshot_backup_volumes command and alter create_instance and create_volume commands to write info servers.js.
      * Some other miscellaneous improvements (e.g. instance_type argument to create_instance).

--- aws/test_manager.py
+++ aws/test_manager.py
@@ -17,9 +17,18 @@ class TestManager:
 
     # this costs money to run :)
     def test_03_create(self):
+        print('WARNING: if test fails ensure created instance termination to avoid running up charges')
         # TODO: could test for eu-west-1 too
         manager = Manager('us-west-1')
-        instance = manager.create_instance()
-        assert instance.id
+        instance, infodict = manager.create_instance()
+        assert infodict['aws_id'] == instance.id
+        assert infodict['aws_public_dns_name']
         instance.stop()
+
+    def test_04_server_info(self):
+        regionstr = 'eu-west-1'
+        m = Manager(regionstr)
+        assert 'eu3' in m.server_info.keys()
+        aws_id = 'i-2275d655'
+        assert m._get_server_by_instance_id(aws_id) == 'eu3'
 
