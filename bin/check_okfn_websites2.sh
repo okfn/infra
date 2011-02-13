9658f4765955015a66c4c091a02f1947db993d8d
OKF Sysadmin <sysadmin@okfn.org>
2011-02-13 19:49:37 +0000

    proper DNS caching in check_okfn_websites2.sh

--- bin/check_okfn_websites2.sh
+++ bin/check_okfn_websites2.sh
@@ -5,12 +5,16 @@
 
 
 DOMAIN="okfn.org"
-MAX_HOST=15
-HOST_NAME[15]="us1" # EXPLICITLY configure names of non "euXY" server hostnames
-APACHE="0 1 2 3 4 5 7 8 9 13 14 15"
+MAX_HOST=16
+HOST_NAME[16]="us1" # EXPLICITLY configure names of non "euXY" server hostnames
+APACHE="0 1 2 3 4 5 7 8 9 13 14 15 16"
 NGINX="10 12"
 SQUID="6"
 NO_OKFN="6"  # Servers where role account "okfn" does not work
+
+
+IGNORE_HOSTNAME="^(localhost|_|WILDCARD) "
+IGNORE_ORIGIN="^(river)$"
 
 
 WARNING="10"
@@ -26,11 +30,20 @@ for i in $ALL; do
 done
 
 
+stderr() {
+  echo -n "${@}" 1>&2
+}
+
+stderrn() { 
+  echo   "${@}" 1>&2
+}
+
+
 if [ -z "${1}" ] ; then
     NO_OKFN_USER="${USER}"
     NO_OKFN_SERVERS=`for s in ${NO_OKFN}; do echo -n ${HOST_NAME[$s]}; done`
-    echo "NOTE: I assume you want to use your current username \"${NO_OKFN_USER}\" to log into the servers which do not" 1>&2
-    echo "      accept the generic username \"okfn\" (${NO_OKFN_SERVERS}). Otherwise tell me as first argument." 1>&2
+    stderrn "NOTE: I assume you want to use your current username \"${NO_OKFN_USER}\" to log into the servers which do not" 
+    stderrn "      accept the generic username \"okfn\" (${NO_OKFN_SERVERS}). Otherwise tell me as first argument." 
 else 
     NO_OKFN_USER="${1}"
 fi
@@ -41,15 +54,15 @@ done
 
 UNKNOWN_SERVER="OTHERHOST"
 UNRESOLVED="DOES_NOT_RESOLV"
-IGNORE="^(localhost|_|WILDCARD) "
 
 
 DIR="/tmp/okfn-webservice-check"
 CACHE_SERVERS="${DIR}/okfn-webservice-01-server-ips"
 CACHE_SERVICES="${DIR}/okfn-webservice-02-configured"
-CACHE_RESOLVED="${DIR}/okfn-webservice-03-resolved"
-CACHE_MAPPING="${DIR}/okfn-webservice-04-mapping"
-CACHE_NAGIOS="${DIR}/okfn-webservice-05-nagios"
+CACHE_SERVICEIPS="${DIR}/okfn-webservice-03-service-ips"
+CACHE_RESOLVED="${DIR}/okfn-webservice-04-resolved"
+CACHE_MAPPING="${DIR}/okfn-webservice-05-mapping"
+CACHE_NAGIOS="${DIR}/okfn-webservice-06-nagios"
 SSH="ssh -4"
 mkdir -p ${DIR}
 
@@ -63,7 +76,7 @@ HOST_PATTERN2="[[:space:]]*has address[[:space:]]*" ;
 HOST_PATTERN="[[:space:]]+(has address|A)[[:space:]]+" ; 
 
 resolv() {
-  host -t a ${1}. 2>&1 | egrep "${HOST_PATTERN}" | sed -e "s/^.*${HOST_PATTERN1}//g" -e "s/^.*${HOST_PATTERN2}//g" | tail -1
+    host -t a ${1}. 2>&1 | grep -v "A record currently not present" | egrep "${HOST_PATTERN}" | sed -e "s/^.*${HOST_PATTERN1}//g" -e "s/^.*${HOST_PATTERN2}//g" | tail -1
 }
 
 
@@ -72,17 +85,17 @@ resolv() {
 # Part #1: resolv names of our servers {eu,us}*.okfn.org to IP addresses:
 #
 if [ -r ${CACHE_SERVERS} ] ; then
-    echo "NOTE: using cached server IPs from ${CACHE_SERVERS} , remove this file to force new DNS lookup of servers" 1>&2
+    stderr "NOTE: using cached server IPs from ${CACHE_SERVERS} , remove this file to force new DNS lookup of servers" 
     while read j ip; do HOST_IP[$j]=${ip}; done < ${CACHE_SERVERS}
 else
-    echo    "NOTE: resolving hostnames of servers, caching results in ${CACHE_SERVERS}" 1>&2
-    echo -n "      " 1>&2
+    stderrn    "NOTE: resolving hostnames of servers, caching results in ${CACHE_SERVERS}" 
+    stderr "      " 
     for i in `seq 0 1 ${MAX_HOST}` ; do
-        echo -n "${HOST_NAME[$i]} " 1>&2
+        stderr "${HOST_NAME[$i]} " 
         HOST_IP[$i]=`resolv ${HOST_NAME[$i]}.${DOMAIN}`
         echo "$i ${HOST_IP[$i]}" 
     done > ${CACHE_SERVERS}
-    echo " " 1>&2
+    stderrn " " 
 fi
 # Check result:
 # for i in `seq 0 1 ${MAX_HOST}` ; do echo "${HOST_NAME[$i]} ${HOST_IP[$i]}" ; done
@@ -92,23 +105,28 @@ fi
 # Part #2: Look into the apache/nginx/squid configs of all our hosts
 #
 
-if [ -r ${CACHE_SERVICES} ] ; then
-    echo "NOTE: using cached service definitions from ${CACHE_SERVICES} , remove this file to force new gathering from servers" 1>&2
-else
-    echo "NOTE: Gathering service definitions from servers, caching results in ${CACHE_SERVICES}" 1>&2
-    echo -n "      " 1>&2
+remove_newline() {
+    echo $@
+}
 
-    echo -n "SQUID: " 1>&2
+
+if [ -r ${CACHE_SERVICES} ] ; then
+    stderrn "NOTE: using cached service definitions from ${CACHE_SERVICES} , remove this file to force new gathering from servers" 
+else
+    stderrn "NOTE: Gathering service definitions from servers, caching results in ${CACHE_SERVICES}" 
+    stderr "      " 
+
+    stderr "SQUID: " 
     for s in ${SQUID}; do
-        echo -n "${HOST_NAME[$s]} " 1>&2
+        stderr "${HOST_NAME[$s]} " 
         ${SSH} ${HOST_USER[$s]}@${HOST_NAME[$s]}.${DOMAIN} egrep "^[[:space:]]*acl[[:space:]]+[^[:space:]]*_sites[[:space:]]*dstdomain[[:space:]]*" /etc/squid3/squid.conf |\
         sed -e "s/_sites/ ${HOST_NAME[$s]}/g" | awk '{ print $5 " " $3 " Cache " $2 }'
-    done | egrep -v $IGNORE >> ${CACHE_SERVICES}
+    done | egrep -v $IGNORE_HOSTNAME >> ${CACHE_SERVICES}
 
 
-    echo -n ";  NGINX: " 1>&2
+    stderr ";  NGINX: " 
     for s in ${NGINX}; do
-        echo -n "${HOST_NAME[$s]} " 1>&2
+        stderr "${HOST_NAME[$s]} " 
         ${SSH} ${HOST_USER[$s]}@${HOST_NAME[$s]}.${DOMAIN} egrep -rh \'^[[:space:]]*server_name[[:space:]]+\' /etc/nginx/sites-enabled/  |\
         sed -e 's/;.*$//' -e 's/\*/WILDCARD/' -e "s/^.*server_name[[:space:]]*\(.*\)/\1/g" |\
         while read name aliases ; do 
@@ -117,45 +135,83 @@ else
                 echo "$alias ${HOST_NAME[$s]} Alias" 
             done
         done
-    done | egrep -v $IGNORE >> ${CACHE_SERVICES}
+    done | egrep -v $IGNORE_HOSTNAME >> ${CACHE_SERVICES}
 
 
-    echo -n ";  APACHE: " 1>&2
+    stderr ";  APACHE: " 
     for s in $APACHE; do
-        echo -n "${HOST_NAME[$s]} " 1>&2
-        ${SSH} ${HOST_USER[$s]}@${HOST_NAME[$s]}.${DOMAIN} egrep -hr \'^[[:space:]]*Server\(Name\|Alias\)[[:space:]]+\' /etc/apache2/sites-enabled/ | \
+        stderr "${HOST_NAME[$s]} " 
+        APACHE_CONFIG="/etc/apache2/sites-enabled/"
+        APACHE_INCLUDES=`${SSH} ${HOST_USER[$s]}@${HOST_NAME[$s]}.${DOMAIN} egrep -hr \'^[[:space:]]*Include[[:space:]]+\' ${APACHE_CONFIG} | \
+        awk '{print $2}' | sort -u`
+        APACHE_CONFIG+=" `remove_newline ${APACHE_INCLUDES}`"
+        # stderr "==>${APACHE_CONFIG}<=="
+
+        ${SSH} ${HOST_USER[$s]}@${HOST_NAME[$s]}.${DOMAIN} egrep -hr \'^[[:space:]]*Server\(Name\|Alias\)[[:space:]]+\' ${APACHE_CONFIG} | \
         sed -e "s/^[[:space:]]*Server\([NameAlias]*\)[[:space:]]*\(.*\)/\1 \2/g" -e 's/\*/WILDCARD/' | \
         while read type names; do
             for name in $names; do 
                 echo "$name ${HOST_NAME[$s]} $type"
             done
         done
-    done | egrep -v $IGNORE >> ${CACHE_SERVICES}
+    done | egrep -v $IGNORE_HOSTNAME >> ${CACHE_SERVICES}
 
-    echo " " 1>&2
+    stderrn " " 
 
 fi
 
 
 #######
-# Part #3: Resolve all service domainnames and compare with server hostnames 
+# Part #3: Resolve all service domainnames to IP addresses 
+#
+
+if [ -r ${CACHE_SERVICEIPS} ] ; then
+    stderrn "NOTE: Reading cached service IP addresses from ${CACHE_SERVICEIPS} and caching new results there."
+    stderrn "      Remove this file to force new DNS lookup of all services" 
+else
+    stderrn "NOTE: Resolving service domain names to IP addresses, caching results in ${CACHE_SERVICEIPS}"
+fi
+
+stderr      "      " 
+
+cat ${CACHE_SERVICES} | awk '{print $1}' | sort -u |\
+while read webdomain; do 
+    if grep -q "^${webdomain} " ${CACHE_SERVICEIPS}; then 
+        stderr "."
+    else
+        webip=`resolv ${webdomain}`
+        if [ "X${webip}" = "X" ] ; then
+            stderr "-" 
+            echo "${webdomain} ${UNRESOLVED}"
+        else
+            stderr "x" 
+          # stderrn "x ${webdomain} x ${webip} x"
+            echo "${webdomain} ${webip}"
+        fi
+      fi
+done >> ${CACHE_SERVICEIPS}
+sort -o ${CACHE_SERVICEIPS} ${CACHE_SERVICEIPS}
+stderrn  " DONE" 
+
+
+#######
+# Part #4: Compare IP addresses of services and servers
 #
 
 if [ -r ${CACHE_RESOLVED} ] ; then
-    echo "NOTE: using cached service domain name resolutions ${CACHE_RESOLVED} , remove this file to force new DNS lookup" 1>&2
+    stderrn "NOTE: using cached comparison of service and server IP addresses from ${CACHE_RESOLVED} , remove this file to force new comparison" 
 else
-    echo    "NOTE: Resolving service domain names and comparing them with server IPs, caching results in ${CACHE_RESOLVED}" 1>&2
-    echo -n "      " 1>&2
+    stderrn "NOTE: Comparing service and server IP addresses, caching results in ${CACHE_RESOLVED}" 
+    stderr  "      " 
 
-    cat ${CACHE_SERVICES} | awk '{print $1}' | sort -u |\
-    while read webdomain; do 
+    cat ${CACHE_SERVICEIPS} | sort -u |\
+    while read webdomain webip; do 
         webhost="${UNKNOWN_SERVER}"
-        webip=`resolv ${webdomain}`
-        if [ "X${webip}" = "X" ] ; then
-            echo -n "x" 1>&2
+        if [ "${webip}" = "${UNRESOLVED}" ] ; then
+            stderr "-" 
             echo    "${webdomain} ${UNRESOLVED}"
-        elif echo "X${webip}" | grep "^X10\." ; then
-            echo -n "p" 1>&2
+        elif echo "X${webip}" | grep -q "^X10\." ; then
+            stderr "p" 
             echo    "${webdomain} RFC1918"
         else
             for i in `seq 0 1 ${MAX_HOST}` ; do
@@ -165,41 +221,42 @@ else
                 fi
             done
             if [ ${webhost} = ${UNKNOWN_SERVER} ] ; then
-                echo -n "." 1>&2
+                stderr "o" 
             else
-                echo -n "o" 1>&2
+                stderr "x" 
             fi
             echo "${webdomain} ${webhost}"
         fi
+        # stderrn "${webdomain} YY ${webip} ZZ ${webhost}"
     done > ${CACHE_RESOLVED}
-    echo  "DONE" 1>&2
+    stderrn  " DONE" 
 fi
 
 
 #######
-# Part #4: Check whether DNS and service definitions match, and print matches
+# Part #5: Check whether DNS and service definitions match, and print matches
 #
 
 if [ -r ${CACHE_MAPPING} ] ; then
-    echo "NOTE: using cached service-to-server mappings from ${CACHE_MAPPING} , remove this file to force fresh mapping" 1>&2
+    stderrn "NOTE: using cached service-to-server mappings from ${CACHE_MAPPING} , remove this file to force fresh mapping" 
 else
-    echo "NOTE: Mapping services to servers, caching results in ${CACHE_RESOLVED}" 1>&2
-    echo " " 1>&2
+    stderrn "NOTE: Mapping services to servers, caching results in ${CACHE_RESOLVED}" 
+    stderrn " " 
 
     cat ${CACHE_RESOLVED} | while read domain res_server; do
         if [ "${res_server}" = "${UNRESOLVED}" ]; then
-            echo -n "    WARN: ${domain} is configured on " 1>&2
-            egrep "^${domain} *" ${CACHE_SERVICES} | grep -v "${UNRESOLVED}" | while read d s rest; do echo -n "${s}," ; done 1>&2 
-            echo " but does not resolv!" 1>&2
+            egrep "^${domain} *" ${CACHE_SERVICES} | grep -v "${UNRESOLVED}" | while read d s rest; do 
+              stderrn "    WARN: ${s} : ${domain} is configured here but does not resolv!" ; 
+            done
         elif [ "${res_server}" = "${UNKNOWN_SERVER}" ]; then
             echo "${domain} ${res_server} Unknown ${res_server}"
         elif [ "${res_server}" = "RFC1918" ]; then
-            echo "    INFO: ${domain} resolves to RFC1918 IP address, ignoring" 1>&2
+            stderrn "    INFO: ${domain} resolves to RFC1918 IP address, ignoring" 
         else
             #echo "${domain}"
             res_line=`egrep "^${domain} *${res_server} *" ${CACHE_SERVICES}`
             if [ "X${res_line}" = "X" ]; then 
-                echo "    WARN: ${domain} is pointing to ${res_server} but is not configured there!" 1>&2
+              stderrn "    WARN: ${res_server} : ${domain} is pointing here but ${res_server} does not feel responsible!" 
             fi
             echo "${res_line}" |while read d r type origin ; do
                 #echo "D=${domain} R=${res_server} d=$d r=$r type=$type origin=$origin"
@@ -209,40 +266,44 @@ else
                         ;;
 
                     "Cache")
-                        found="0"
-                        grep "^${domain} *${origin}" ${CACHE_SERVICES} | while read d2 o2 type2 ; do
-                            found="1"
-                            echo "${domain} ${res_server} ${type2} ${origin}"
-                        done
-                        if [ ${found} = "0" ]; then
-                            echo "    WARN: ${domain} is pointing to proxy ${res_server}, but origin ${origin} does not feel responsible!" 1>&2
-                        fi
+                        origin_service=`grep "^${domain} *${origin}" ${CACHE_SERVICES}`
+                        if [ "X${origin_service}" = "X" ] ; then
+                          # stderrn "if ! echo \"${origin}\" | egrep -q \"${IGNORE_ORIGIN}\" ; then"
+                            if ! echo "${origin}" | egrep -q "${IGNORE_ORIGIN}" ; then
+                                stderrn "    WARN: ${origin} : ${domain} is pointing to proxy ${res_server}, but origin ${origin} does not feel responsible!" 
+                            fi
+                        else 
+                            echo "${origin_service}" | while read d2 o2 type2 ; do
+                                echo "${domain} ${res_server} ${type2} ${origin}"
+                              # stderrn "   FOUND: ${domain} ${res_server} ${type2} ${origin}"
+                            done
+                        fi 
                         ;;
 
                     "*")
-                        echo "    WARN: ${domain} of unknown type ${type}!" 1>&2
+                        stderrn "    WARN: ${domain} of unknown type ${type}!" 
                         ;;
                 esac
                 if [ "${origin}" = "" ]; then origin=${res_server}; fi
                 egrep "^${domain} " ${CACHE_SERVICES} | egrep -v "^${domain} *${origin} " | grep -v " Cache "| while read d r t o ; do
-                    echo "    INFO: ${domain} is configured on ${r}, but neither DNS nor proxy are pointing to that." 1>&2
+                    stderrn "    INFO: ${r} : ${domain} is configured on ${r}, but neither DNS nor proxy are pointing to that." 
                 done
             done
         fi
     done > ${CACHE_MAPPING}
-    echo " " 1>&2
+    stderrn " " 
 fi
 
 
 #######
-# Part #5: Create Nagios service definitions from our mapping
+# Part #6: Create Nagios service definitions from our mapping
 #
 
 if [ -r ${CACHE_NAGIOS} ] ; then
-    echo "NOTE: Nagios config ${CACHE_NAGIOS} already there. Remove this file to write a fresh one" 1>&2
+    stderrn "NOTE: Nagios config ${CACHE_NAGIOS} already there. Remove this file to write a fresh one" 
 else
-    echo "NOTE: Using services-to-servers mapping to generate Nagios configuration file ${CACHE_NAGIOS} " 1>&2
-    echo " " 1>&2
+    stderrn "NOTE: Using services-to-servers mapping to generate Nagios configuration file ${CACHE_NAGIOS} " 
+    stderrn " " 
 
 
 ###
@@ -263,7 +324,7 @@ EOF
             check="check_http2!${domain}!${WARNING}!${CRITICAL}"
         fi
         if [ ${type} = "Alias" ] ; then
-            echo "      INFO: ${domain} is only a ServerAlias, not adding to Nagios config." 1>&2
+            stderrn "      INFO: ${domain} is only a ServerAlias, not adding to Nagios config." 
         else
             # Check origin
 ####
