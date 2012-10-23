33e18f32c35166cce6ddb7a66302671c2f8faa43
Friedrich Lindenberg <friedrich@pudo.org>
2012-10-23 11:01:49 +0000

    Make it web-scale

--- tools/backup_textus_bonsai.sh
+++ tools/backup_textus_bonsai.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ES_HOST=index.bonsai.io
-ES_INDEX=`cat /root/heroku_es_index`
 
-/home/okfn/hg-sysadmin/tools/backup-es.sh $ES_HOST $ES_INDEX
 mkdir -p $1
-mv $ES_INDEX-documents-* $1
-mv $ES_INDEX-mappings-* $1
+
+while read ES_INDEX; do
+	/home/okfn/hg-sysadmin/tools/backup-es.sh $ES_HOST $ES_INDEX &>/dev/null
+	mv $ES_INDEX-documents-* $1
+	mv $ES_INDEX-mappings-* $1
+done < /root/heroku_es_index
 
 
