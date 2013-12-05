b34594c2e6f7b3b702fb1d6bc5cd9c1b5a0da266
William Waites <wwaites@tardis.ed.ac.uk>
2013-12-05 22:11:31 +0000

    RT_SiteConfig.pm now managed by ansible. Closes RT#231

---
+++ ansible/roles/rt/files/RT_SiteConfig.pm
@@ -0,0 +1,24 @@
+Set( $rtname, "okfn.org" );
+Set( $Organization, "okfn.org" );
+Set( $WebPath, "" );
+Set( $WebPort, 443 );
+Set( $WebDomain, "rt.okfn.org" );
+Set( $WebSecureCookies, 1 );
+Set( $DatabaseHost, $ENV{DB_PORT_5432_TCP_ADDR} );
+Set( $DatabasePassword, $ENV{RT_DATABASE_PW} );
+Set( $Timezone, "UTC" );
+Set( $MaxInlineBody, 120000 );
+
+Plugin( "RT::Extension::ActivityReports" );
+Plugin( "RT::Extension::ResetPassword" );
+Plugin( "RT::Extension::MergeUsers" );
+Plugin( "RT::Extension::SpawnLinkedTicketInQueue" );
+
+Plugin( "RT::Extension::CommandByMail" );
+Set( @MailPlugins, qw(Auth::MailFrom Filter::TakeAction) );
+Set( $CommandByMailHeader, "X-OKF-UZOQU0" );
+
+Plugin( "RT::Extension::RepeatTicket" );
+Set( $RepeatTicketCoexistentNumber, 1 ); # Optional
+Set( $RepeatTicketLeadTime, 14 ); # Optional
+Set( $RepeatTicketSubjectFormat, '__Subject__' );
