9b1b326aec3e34bae3fd7bf124201795c89f4c13
Joel Rebello <joel.rebell@gmail.com>
2013-12-11 13:58:10 +0000

    update script and script config for port definition

--- ansible/roles/scripts/files/opt/scripts/backup/mysql_backup.sh
+++ ansible/roles/scripts/files/opt/scripts/backup/mysql_backup.sh
@@ -3,13 +3,26 @@
 #this config file is placed by ansible, look in the scripts role for mysql related scripts
 backup_config=/opt/scripts/backup/mysql_backup.conf
 backup_dir=/root/mysql_backups
-
+db_host=localhost
+db_port=3306
 
 if [ -f $backup_config ];
 then
 	rsync_target=$(awk -F: /backup_rsync_target/'{print $2}' $backup_config | sed -e 's/^ //g')
-        db_list=$(awk -F: /db_backup_list/'{print $2}' ${backup_config} | sed -e 's/^ //g')	
+        db_list=$(awk -F: /mysql_backup_list/'{print $2}' ${backup_config} | sed -e 's/^ //g')
 	
+	port=$(awk -F: /mysql_port/'{print $2}' ${backup_config} | sed -e 's/^ //g')	
+	if [ ! -z "${port}" ]; 
+	then
+		db_port=${port}
+	fi
+
+	host=$(awk -F: /mysql_host/'{print $2}' ${backup_config} | sed -e 's/^ //g')	
+	if [ ! -z "${host}" ];
+	then
+		db_host=${host}
+	fi
+
 	if [ ! -d ${backup_dir} ];
 	then
 		mkdir -p ${backup_dir}
@@ -23,7 +36,7 @@ then
 		ts=$(date +%s)
 		backup_archive="${ts}-$(hostname -s)-${db}-sql.gz"
 		
-		/usr/bin/mysqldump ${db} | gzip > ${backup_dir}/${backup_archive} 
+		/usr/bin/mysqldump -uroot -P${db_port} -h${db_host} ${db} | gzip > ${backup_dir}/${backup_archive} 
 		
 		if [ -s ${backup_dir}/${backup_archive} ];
 		then
