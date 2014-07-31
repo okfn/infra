22149749deb1d2d8b6300ce33d49fb494b43c6c0
Joel Rebello <joel.rebell@gmail.com>
2014-07-31 13:28:55 +0000

    graphite collectors: update script to collec greylist stats

--- ansible/roles/scripts/files/opt/scripts/graphite_collectors/postfix_spam_stats.py
+++ ansible/roles/scripts/files/opt/scripts/graphite_collectors/postfix_spam_stats.py
@@ -24,6 +24,8 @@ def get_spam_stats():
 #	stats['queue_size'] = 0
 	stats['spamhaus_blocks'] = 0
 	stats['spamasassin_rejects'] = 0
+	stats['greylist_rejects'] = 0
+	stats['greylist_allow'] = 0
 
 	time_now = int(time.time())
 	five_mins_ago = time_now-300
@@ -33,20 +35,28 @@ def get_spam_stats():
 		if not line:
 			continue
 
-		m = re.match(r"(?P<ts>\w+ \d+ \d+:\d+:\d+).*: (?:554.*(?P<spamhaus>blocked)|(?P<spamd>spamd): result: Y)", line)
-		if m:
-			ts = m.group('ts')
-			ts = ts + ' ' + year
-			ts = int(time.mktime(time.strptime(ts, '%b %d %H:%M:%S %Y')))
-			if ts < five_mins_ago:
-				break
-			
-			if m.group('spamhaus'):
-				stats['spamhaus_blocks'] += 1
-			
-			if m.group('spamd'):
-				stats['spamasassin_rejects'] += 1
-
+		m = re.match(r"(?P<ts>\w+ \d+ \d+:\d+:\d+).*$", line)
+		if m:  
+		        ts = m.group('ts')
+		        ts = ts + ' ' + year
+		        ts = int(time.mktime(time.strptime(ts, '%b %d %H:%M:%S %Y')))
+		        if ts < five_mins_ago:
+		                break
+	
+		m = re.match(r"^\w+ \d+ \d+:\d+:\d+.*: (?:554.*(?P<spamhaus>blocked)|(?P<spamd>spamd): result: Y)", line)
+		if m:  
+		        if m.group('spamhaus'):
+		                stats['spamhaus_blocks'] += 1
+		
+		        if m.group('spamd'):
+		                stats['spamasassin_rejects'] += 1
+		
+		m = re.match(r"\w+ \d+ \d+:\d+:\d+.* postgrey\[\d+\]: action=(:?(?P<greylist>greylist)|(?P<pass>pass)),.*", line)
+		if m:  
+		        if m.group('greylist'):
+		                stats['greylist_rejects'] += 1
+		        if m.group('pass'):
+		                stats['greylist_allow'] += 1
 			
 	push_stats(stats)	
 
